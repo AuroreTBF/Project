f581fb86906542425b81577b123c7c1e40d1dd89
AuroreTBF <archlee196@gmail.com>
2023-12-29 01:08:35 +0100

    hh

--- Sign up/Signup.php
+++ Sign up/Signup.php
@@ -43,9 +43,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   if(empty($_POST['email'])){
     $emailErr = "Email is required";
   }else {
-    $email = $_POST['email'];
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format";
+    }
+    else{
+      $email = $_POST['email'];
     }
   }
   if(empty($_POST['address'])){
