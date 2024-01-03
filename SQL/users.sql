73e332fbd925e0526e1d7d41317db14eb73958c0
AuroreTBF <archlee196@gmail.com>
2024-01-03 03:58:54 +0100

    hhhh

--- SQL/users.sql
+++ SQL/users.sql
@@ -1,16 +1,16 @@
 CREATE TABLE USERS;
-INSERT INTO users (id,firstname, lastname, email, address,date,password)
+INSERT INTO users (firstname, lastname, email, address, date, password)
 VALUES 
-    (NULL,'Alice', 'Johnson', 'alice.johnson@example.com', '123 Main St, Cityville',CURRENT_DATE ,'21bd12dc183f740ee76f27b78eb39c8ad972a757'),
-    (NULL,'Bob', 'Smith', 'bob.smith@example.com', '456 Elm St, Townsville', CURRENT_DATE ,'473abb1d9044ddfeda1c54b02a0379ce5d50a186'),
-    (NULL,'Charlie', 'Williams', 'charlie.williams@example.com', '789 Oak St, Villageton', CURRENT_DATE ,'0d0c65e86c444a039b7cadc6f83ee3708cdb9660'),
-    (NULL,'David', 'Brown', 'david.brown@example.com', '321 Pine St, Hamlet City',CURRENT_DATE , '0cae59d777b5a29a0aec9cf1817861929b5cb5e0'),
-    (NULL,'Emma', 'Miller', 'emma.miller@example.com', '654 Birch St, Countryside',CURRENT_DATE , '076d3e6c4b9f654b5b220b9045b7458ab6b4cbc6'),
-    (NULL,'Frank', 'Davis', 'frank.davis@example.com', '987 Cedar St, Township',CURRENT_DATE , '40f382599efa867c48b47aba945db44f7acf9a24'),
-    (NULL,'Grace', 'Garcia', 'grace.garcia@example.com', '246 Maple St, Villageton',CURRENT_DATE , 'fb192a6a14a580b7a9f22d69916b39b442c1f5c9'),
-    (NULL,'Henry', 'Martinez', 'henry.martinez@example.com', '135 Walnut St, Riverside',CURRENT_DATE , '02726d40f378e716981c4321d60ba3a325ed6a4c'),
-    (NULL,'Isabella', 'Lopez', 'isabella.lopez@example.com', '864 Cherry St, Suburbia',CURRENT_DATE , '9f447054819975f30a80e593961d850788edf9b4'),
-    (NULL,'Jack', 'Hernandez', 'jack.hernandez@example.com', '579 Magnolia St, Cityscape',CURRENT_DATE , '1970c2c411d2b8fcf061f99b307bbbe26181ea5f');
+    ('Alice', 'Johnson', 'alice.johnson@example.com', '123 Main St, Cityville',CURRENT_DATE ,'21bd12dc183f740ee76f27b78eb39c8ad972a757'),
+    ('Bob', 'Smith', 'bob.smith@example.com', '456 Elm St, Townsville', CURRENT_DATE ,'473abb1d9044ddfeda1c54b02a0379ce5d50a186'),
+    ('Charlie', 'Williams', 'charlie.williams@example.com', '789 Oak St, Villageton', CURRENT_DATE ,'0d0c65e86c444a039b7cadc6f83ee3708cdb9660'),
+    ('David', 'Brown', 'david.brown@example.com', '321 Pine St, Hamlet City',CURRENT_DATE , '0cae59d777b5a29a0aec9cf1817861929b5cb5e0'),
+    ('Emma', 'Miller', 'emma.miller@example.com', '654 Birch St, Countryside',CURRENT_DATE , '076d3e6c4b9f654b5b220b9045b7458ab6b4cbc6'),
+    ('Frank', 'Davis', 'frank.davis@example.com', '987 Cedar St, Township',CURRENT_DATE , '40f382599efa867c48b47aba945db44f7acf9a24'),
+    ('Grace', 'Garcia', 'grace.garcia@example.com', '246 Maple St, Villageton',CURRENT_DATE , 'fb192a6a14a580b7a9f22d69916b39b442c1f5c9'),
+    ('Henry', 'Martinez', 'henry.martinez@example.com', '135 Walnut St, Riverside',CURRENT_DATE , '02726d40f378e716981c4321d60ba3a325ed6a4c'),
+    ('Isabella', 'Lopez', 'isabella.lopez@example.com', '864 Cherry St, Suburbia',CURRENT_DATE , '9f447054819975f30a80e593961d850788edf9b4'),
+    ('Jack', 'Hernandez', 'jack.hernandez@example.com', '579 Magnolia St, Cityscape',CURRENT_DATE , '1970c2c411d2b8fcf061f99b307bbbe26181ea5f');
 
 /*Email: alice.johnson@example.com
 Password: P@ssw0rd
